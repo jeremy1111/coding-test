3a038a7fb07391ef4e8a8a9db4ad305420d16ea8
Jeremy Hui <jk.hui@hotmail.com>
2019-06-03 22:46:53 +0100

    fourth commit

--- find_distance.py
+++ find_distance.py
@@ -4,9 +4,12 @@ import math
 
 
 # Function found in Stackoverflow to mathematically compute distance in km 
-# between two points based on longitude and latitude 
-def distance(lat1, lon1, lat2, lon2):
-    p = math.pi / 180
-    a = (0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) 
-         * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2)
-    return 12742 * asin(sqrt(a))
+# between two points given their longitude and latitude (Haversine formula)
+def distance(latitude1, longitude1, latitude2, longitude2):
+    circumference = math.pi / 180
+    earth_radius = 6371
+    long_equation = (0.5 - cos((latitude2 - latitude1) * circumference)/2 
+         + cos(latitude1 * circumference) * cos(latitude2 * circumference) 
+         * (1 - cos((longitude2 - longitude1) * circumference)) / 2)
+    distance_pt1_and_pt2 = 2 * earth_radius * asin(sqrt(long_equation))
+    return distance_pt1_and_pt2
