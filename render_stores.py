572cc92f4968444b4022bdbb777ce1b848b485d3
jeremy1111 <jk.hui@hotmail.com>
2019-06-03 15:18:34 +0100

    third commit in line with pep8

--- render_stores.py
+++ render_stores.py
@@ -4,20 +4,20 @@ import requests
 
 
 
-with open("stores.json") as list_of_stores:    
-    listOfStores = json.load(list_of_stores)
-    listOfStoresInTuples = []
-    for index in range(len(listOfStores)):
-        name = listOfStores[index]['name']
-        postcode = listOfStores[index]['postcode']
-        listOfStoresInTuples.append((name, postcode))
-    listOfStoresInTuples.sort()
+with open("stores.json") as store_list:    
+    list_of_stores = json.load(store_list)
+    list_of_stores_in_tuples = []
+    for index in range(len(list_of_stores)):
+        name = list_of_stores[index]['name']
+        postcode = list_of_stores[index]['postcode']
+        list_of_stores_in_tuples.append((name, postcode))
+    list_of_stores_in_tuples.sort()
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_list_of_stores():
-    return render_template('render_stores.html', nameAndPostcode = listOfStoresInTuples)
+    return render_template('render_stores.html', name_and_postcode = list_of_stores_in_tuples)
 
 if __name__ == '__main__':
     app.run(debug = True)   
