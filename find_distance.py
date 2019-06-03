984633896e1b4cc73fe5d2b2df02503978abaf85
jeremy1111 <jk.hui@hotmail.com>
2019-06-03 15:19:41 +0100

    third commit in line with pep8

--- find_distance.py
+++ find_distance.py
@@ -3,8 +3,10 @@ import math
 
 
 
-# Function found in Stackoverflow to mathematically compute distance in kilometres between two points based on longitude and latitude 
+# Function found in Stackoverflow to mathematically compute distance in km 
+# between two points based on longitude and latitude 
 def distance(lat1, lon1, lat2, lon2):
     p = math.pi / 180
-    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    a = (0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) 
+         * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2)
     return 12742 * asin(sqrt(a))
